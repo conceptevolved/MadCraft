3b3b924c33530a35d24252c127fa2be4bce405ee
Dave <conceptevolved@gmail.com>
2016-08-20 10:48:55 -0400

    Initial Commit
    Forge and Eclipse setup

---
+++ src/main/java/com/conceptevolved/madcraft/MadCraft.java
@@ -0,0 +1,18 @@
+package com.conceptevolved.madcraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MadCraft.MODID, version = MadCraft.VERSION)
+public class MadCraft
+{
+    public static final String MODID = "madcraft";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
